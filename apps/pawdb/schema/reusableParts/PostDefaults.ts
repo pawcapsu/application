1f5fc3c0437a527348499e8fa412d5ee68e65efe
Huskie Jameson <husky@odzi.dog>
2024-12-02 07:34:12 +0200

    schema: add relation to content_containers from PostDefaults function

--- apps/pawdb/schema/reusableParts/PostDefaults.ts
+++ apps/pawdb/schema/reusableParts/PostDefaults.ts
@@ -1,11 +1,14 @@
 import { Schema as S } from "@triplit/client";
 import { ScrapperInfoPart } from "./ScrapperInfoPart";
+import { ContentModuleTables } from "../modules/Content/ContentModuleSchema";
 
 export function PostDefaults(schema: S) {
   return S.Schema({
     // Post defaults values
     id: S.Id(),
     scrapper_info: S.Optional(ScrapperInfoPart),
+    container_id: S.String(),
+    container: S.RelationById(ContentModuleTables.ContentContainers, "$container_id"),
 
     // User-specified fields
     ...schema,
